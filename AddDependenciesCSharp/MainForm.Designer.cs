e6b1417d453f8b78614af8f6f22b520768a0a7bd
francoishill <francoishill@cf696688-335f-5c42-91fb-8731079d8cc7>
2012-03-24 17:33:41 +0000

    git-svn-id: svn://localhost/adddependenciescsharp/trunk@2 cf696688-335f-5c42-91fb-8731079d8cc7

---
+++ AddDependenciesCSharp/MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace AddDependenciesCSharp
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxProjectPath = new System.Windows.Forms.TextBox();
+			this.buttonBrowseProject = new System.Windows.Forms.Button();
+			this.treeViewItemsToAdd = new System.Windows.Forms.TreeView();
+			this.comboBoxSharedClasses = new System.Windows.Forms.ComboBox();
+			this.radioButtonWinforms = new System.Windows.Forms.RadioButton();
+			this.radioButtonWPF = new System.Windows.Forms.RadioButton();
+			this.radioButtonConsole = new System.Windows.Forms.RadioButton();
+			this.buttonAccept = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBoxProjectPath
+			// 
+			this.textBoxProjectPath.AllowDrop = true;
+			this.textBoxProjectPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxProjectPath.Location = new System.Drawing.Point(12, 12);
+			this.textBoxProjectPath.Name = "textBoxProjectPath";
+			this.textBoxProjectPath.Size = new System.Drawing.Size(785, 20);
+			this.textBoxProjectPath.TabIndex = 1;
+			this.textBoxProjectPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBoxProjectPath_DragDrop);
+			this.textBoxProjectPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBoxProjectPath_DragEnter);
+			// 
+			// buttonBrowseProject
+			// 
+			this.buttonBrowseProject.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonBrowseProject.Location = new System.Drawing.Point(808, 10);
+			this.buttonBrowseProject.Name = "buttonBrowseProject";
+			this.buttonBrowseProject.Size = new System.Drawing.Size(30, 23);
+			this.buttonBrowseProject.TabIndex = 2;
+			this.buttonBrowseProject.Text = "...";
+			this.buttonBrowseProject.UseVisualStyleBackColor = true;
+			// 
+			// treeViewItemsToAdd
+			// 
+			this.treeViewItemsToAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.treeViewItemsToAdd.HotTracking = true;
+			this.treeViewItemsToAdd.Indent = 5;
+			this.treeViewItemsToAdd.Location = new System.Drawing.Point(12, 109);
+			this.treeViewItemsToAdd.Name = "treeViewItemsToAdd";
+			this.treeViewItemsToAdd.ShowLines = false;
+			this.treeViewItemsToAdd.ShowPlusMinus = false;
+			this.treeViewItemsToAdd.ShowRootLines = false;
+			this.treeViewItemsToAdd.Size = new System.Drawing.Size(406, 145);
+			this.treeViewItemsToAdd.TabIndex = 3;
+			// 
+			// comboBoxSharedClasses
+			// 
+			this.comboBoxSharedClasses.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+			this.comboBoxSharedClasses.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+			this.comboBoxSharedClasses.FormattingEnabled = true;
+			this.comboBoxSharedClasses.Location = new System.Drawing.Point(12, 82);
+			this.comboBoxSharedClasses.Name = "comboBoxSharedClasses";
+			this.comboBoxSharedClasses.Size = new System.Drawing.Size(321, 21);
+			this.comboBoxSharedClasses.TabIndex = 4;
+			this.comboBoxSharedClasses.KeyUp += new System.Windows.Forms.KeyEventHandler(this.comboBoxSharedClasses_KeyUp);
+			// 
+			// radioButtonWinforms
+			// 
+			this.radioButtonWinforms.AutoSize = true;
+			this.radioButtonWinforms.Location = new System.Drawing.Point(81, 38);
+			this.radioButtonWinforms.Name = "radioButtonWinforms";
+			this.radioButtonWinforms.Size = new System.Drawing.Size(100, 17);
+			this.radioButtonWinforms.TabIndex = 5;
+			this.radioButtonWinforms.TabStop = true;
+			this.radioButtonWinforms.Text = "Windows Forms";
+			this.radioButtonWinforms.UseVisualStyleBackColor = true;
+			// 
+			// radioButtonWPF
+			// 
+			this.radioButtonWPF.AutoSize = true;
+			this.radioButtonWPF.Location = new System.Drawing.Point(187, 38);
+			this.radioButtonWPF.Name = "radioButtonWPF";
+			this.radioButtonWPF.Size = new System.Drawing.Size(49, 17);
+			this.radioButtonWPF.TabIndex = 6;
+			this.radioButtonWPF.TabStop = true;
+			this.radioButtonWPF.Text = "WPF";
+			this.radioButtonWPF.UseVisualStyleBackColor = true;
+			// 
+			// radioButtonConsole
+			// 
+			this.radioButtonConsole.AutoSize = true;
+			this.radioButtonConsole.Location = new System.Drawing.Point(12, 38);
+			this.radioButtonConsole.Name = "radioButtonConsole";
+			this.radioButtonConsole.Size = new System.Drawing.Size(63, 17);
+			this.radioButtonConsole.TabIndex = 7;
+			this.radioButtonConsole.TabStop = true;
+			this.radioButtonConsole.Text = "Console";
+			this.radioButtonConsole.UseVisualStyleBackColor = true;
+			// 
+			// buttonAccept
+			// 
+			this.buttonAccept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonAccept.Location = new System.Drawing.Point(763, 231);
+			this.buttonAccept.Name = "buttonAccept";
+			this.buttonAccept.Size = new System.Drawing.Size(75, 23);
+			this.buttonAccept.TabIndex = 8;
+			this.buttonAccept.Text = "A&ccept";
+			this.buttonAccept.UseVisualStyleBackColor = true;
+			this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(850, 266);
+			this.Controls.Add(this.buttonAccept);
+			this.Controls.Add(this.radioButtonConsole);
+			this.Controls.Add(this.radioButtonWPF);
+			this.Controls.Add(this.radioButtonWinforms);
+			this.Controls.Add(this.comboBoxSharedClasses);
+			this.Controls.Add(this.treeViewItemsToAdd);
+			this.Controls.Add(this.buttonBrowseProject);
+			this.Controls.Add(this.textBoxProjectPath);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxProjectPath;
+		private System.Windows.Forms.Button buttonBrowseProject;
+		private System.Windows.Forms.TreeView treeViewItemsToAdd;
+		private System.Windows.Forms.ComboBox comboBoxSharedClasses;
+		private System.Windows.Forms.RadioButton radioButtonWinforms;
+		private System.Windows.Forms.RadioButton radioButtonWPF;
+		private System.Windows.Forms.RadioButton radioButtonConsole;
+		private System.Windows.Forms.Button buttonAccept;
+	}
+}
+
---
+++ AddDependenciesCSharp/MainForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using SharedClasses;
+
+namespace AddDependenciesCSharp
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+
+			foreach (FullPathAndDisplayName file in CSharpDependencies.GetSharedClasses())
+				comboBoxSharedClasses.Items.Add(file);
+		}
+
+		private void textBoxProjectPath_DragEnter(object sender, DragEventArgs e)
+		{
+			// Check if the Dataformat of the data can be accepted
+			// (we only accept file drops from Explorer, etc.)
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+				e.Effect = DragDropEffects.Copy; // Okay
+			else
+				e.Effect = DragDropEffects.None; // Unknown data, ignore it
+		}
+
+		private void textBoxProjectPath_DragDrop(object sender, DragEventArgs e)
+		{
+			// Extract the data from the DataObject-Container into a string list
+			string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+			// Do something with the data...
+
+			// For example add all files into a simple label control:
+			if (FileList.Length != 1)
+				UserMessages.ShowWarningMessage("Cannot only use 1 project file");
+			else if (!FileList[0].EndsWith(".csproj", StringComparison.InvariantCultureIgnoreCase))
+				UserMessages.ShowWarningMessage("File must be of type .csproj");
+			else
+			{
+				textBoxProjectPath.Text = FileList[0];
+			}
+		}
+
+		protected override void OnHandleCreated(EventArgs e)
+		{
+			StylingInterop.SetTreeviewVistaStyle(treeViewItemsToAdd);
+			base.OnHandleCreated(e);
+		}
+
+		private void comboBoxSharedClasses_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				if (comboBoxSharedClasses.SelectedIndex != -1 || !string.IsNullOrWhiteSpace(comboBoxSharedClasses.Text))
+					AddComboboxItemToList();
+				else
+					buttonAccept.PerformClick();
+			}
+		}
+
+		private void AddComboboxItemToList()
+		{
+			FullPathAndDisplayName file = comboBoxSharedClasses.SelectedItem as FullPathAndDisplayName;
+			if (file != null)
+			{
+				AddFileToTreeview(file);
+			}
+			else if (!string.IsNullOrWhiteSpace(comboBoxSharedClasses.Text))
+				AddFileToTreeview(new FullPathAndDisplayName(comboBoxSharedClasses.Text));
+			else
+				UserMessages.ShowWarningMessage("Cannot add blank item");
+		}
+
+		private void AddFileToTreeview(FullPathAndDisplayName file)
+		{
+			treeViewItemsToAdd.Nodes.Add(file.FullPath, file.DisplayName).Tag = file;
+			if (comboBoxSharedClasses.SelectedIndex != -1)
+				comboBoxSharedClasses.Items.Remove(file);
+			comboBoxSharedClasses.SelectedIndex = -1;
+			comboBoxSharedClasses.Text = null;
+		}
+
+		private void buttonAccept_Click(object sender, EventArgs e)
+		{
+			if (comboBoxSharedClasses.Text.Length != 0 && UserMessages.Confirm("The current selected item is not added to the list, add it now?"))
+				AddComboboxItemToList();
+
+			if (string.IsNullOrWhiteSpace(textBoxProjectPath.Text))
+				UserMessages.ShowWarningMessage("Please select a project first.");
+			else if (!textBoxProjectPath.Text.EndsWith(".csproj", StringComparison.InvariantCultureIgnoreCase))
+				UserMessages.ShowWarningMessage("Please select a valid .csproj file.");
+			else if (comboBoxSharedClasses.Text.Length != 0)
+				UserMessages.ShowWarningMessage("Please add the item to the list first.");
+			else if (treeViewItemsToAdd.Nodes.Count == 0)
+				UserMessages.ShowWarningMessage("Please add at least one item.");
+			else
+			{
+				//All checks successful, may proceed
+				List<FullPathAndDisplayName> tmplist = new List<FullPathAndDisplayName>();
+				foreach (TreeNode node in treeViewItemsToAdd.Nodes)
+					tmplist.Add(node.Tag as FullPathAndDisplayName);
+				CSharpDependencies.EnsureCorrectFileDependancies(textBoxProjectPath.Text, tmplist.ToArray());
+			}
+		}
+	}
+}
